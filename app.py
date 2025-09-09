ea6bc717fe6fc55b3945d638ddd19ac579e0ca68
VinodKumarBN <vinodvinodkumarbn164@gmail.com>
2025-09-09 13:55:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,5 +51,7 @@ def get_response():   # ✅ renamed
     print("Response:", result["result"])
     return str(result["result"])
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
